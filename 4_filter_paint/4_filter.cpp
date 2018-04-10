a32442f96b840f4d7cc3e2d12409e3a1655a70cf
Ádám Vécsi <adamvecsi@Adam-MacBook-Pro.local>
2018-04-10 12:39:33 +0200

    new filter

--- 4_filter_paint/4_filter.cpp
+++ 4_filter_paint/4_filter.cpp
@@ -68,27 +68,3 @@ int main(int argc, char** argv)
     
     return 0;
 }
-
-/*
-def comic(img):
-# do edge detection on a grayscale image
-gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
-edges = cv2.blur(gray, v) # this blur gets rid of some noise
-edges = cv2.Canny(edges, 50, 150, apertureSize=3) # this is the edge detection
-
-# the edges are a bit thin, this blur and threshold make them a bit fatter
-kernel = numpy.ones((3,3), dtype=numpy.float) / 12.0
-edges = cv2.filter2D(edges, 0, kernel)
-edges = cv2.threshold(edges, 50, 255, 0)[1]
-
-# and back to colour...
-edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-
-# this is the expensive operation, it blurs things but keeps track of
-# colour boundaries or something, heck, just play with it
-shifted = cv2.pyrMeanShiftFiltering(img, 5, 20)
-
-# now compose with the edges, the edges are white so take them away
-# to leave black
-return cv2.subtract(shifted, edges)
-*/
